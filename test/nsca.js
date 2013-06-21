043b447a33058ecc38a558599a3104829b5c2abf
Josh Toft <joshtoft@gmail.com>
2013-06-21 07:02:13 -0700

    Saving config state

--- test/nsca.js
+++ test/nsca.js
@@ -1,7 +1,11 @@
 var dgram = require('dgram');
+var NSCA  = require('../lib/nsca');
 
 describe('nsca', function() {
-  it("should have some tests soon", function(done) {
+  it("should be able to read send_nsca configs", function(done) {
+    var nsca = new NSCA("./fixtures/send_nsca.cfg");
+    nsca.password.should.equal("myrandompassword");
+    nsca.encryption.should.equal(1);
     done();
   });
 });
