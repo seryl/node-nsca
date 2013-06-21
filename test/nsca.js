83fff79cb4168937eae906fdd6a92b75916a8a1d
Josh Toft <joshtoft@gmail.com>
2013-06-21 15:51:20 -0700

    Added configuration testing

--- test/nsca.js
+++ test/nsca.js
@@ -3,7 +3,7 @@ var NSCA  = require('../lib/nsca');
 
 describe('nsca', function() {
   it("should be able to read send_nsca configs", function(done) {
-    var nsca = new NSCA("./fixtures/send_nsca.cfg");
+    var nsca = new NSCA(__dirname + "/fixtures/send_nsca.cfg");
     nsca.password.should.equal("myrandompassword");
     nsca.encryption.should.equal(1);
     done();
